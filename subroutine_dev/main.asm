e9dafd5399662113ad3fc10c0769824e016f327d
Nolan Larison <nlarison@Nolans-MBP.attlocal.net>
2024-03-08 07:45:58 -0500

    Added 32 bit multiplication srt

--- subroutine_dev/main.asm
+++ subroutine_dev/main.asm
@@ -9,16 +9,11 @@
             .def    RESET                   ; Export program entry-point to
                                             ; make it known to linker.
 ;-------------------------------------------------------------------------------
-            .data                           ; Assemble into program memory.
-            .retain                         ; Override ELF conditional linking
-            .retainrefs                     ; And retain any sections that have
-
-
-
-;-------------------------------------------------------------------------------
             .text                           ; Assemble into program memory.
             .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
             .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
 
 ;-------------------------------------------------------------------------------
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
@@ -29,23 +24,24 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 
-; Enter your code here
-			mov.w SP, R5
-			mov.w #0, 2(SP)
-			mov.w #1, 4(SP)
-			mov.w #5, 6(SP)
 
-loop:
-			mov.w 2(SP), R4
-			add.w 4(SP), R4
 
-			mov.w 4(SP), 2(SP)
-			mov.w R4, 4(SP)
+			push #10
+			push #250
+			push #2
+			push #24
+			decd.w SP
+			decd.w SP
+			call #x_times_y
 
-			dec.w 6(SP)
 
-			tst 4(SP)
-			jnz loop
+
+
+			pop R4
+			pop R5
+			add.w #8, SP
+
+
 
 
 
@@ -54,6 +50,56 @@ loop:
 
 main:		jmp main
 			nop
+;-------------------------------------------------------------------------------
+; Subroutine: x_times_y
+; Stack Frame:
+;				|	PC	| <-SP
+; 				|  f[n] | <-4(SP)
+; 				|	x	| <-8(SP)
+; 				|	y	| <-12(SP)
+;
+;
+; Function takes two 32 bit unsigned integers in the stack and returns a 32 bit
+; signed int to the stack
+;-------------------------------------------------------------------------------
+x_times_y:
+			decd.w SP
+			clr.w 4(SP)
+			clr.w 6(SP)
+
+loop:
+			tst 10(SP)
+			jnz skp_tst
+			tst 8(SP)
+			jz end
+skp_tst:
+			bit.w #1, 8(SP)
+			jz rolls
+
+			add.w 12(SP), 4(SP)
+			addc.w 14(SP), 6(SP)
+
+rolls:
+			clrc
+			rrc.w 10(SP)
+			rrc.w 8(SP)
+
+			clrc
+			rlc.w 12(SP)
+			rlc.w 14(SP)
+			jmp loop
+
+
+end:
+			incd.w SP
+			ret
+
+
+
+
+
+
+
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
