d5b2c27fef90c26d872d84298551bf18218c7948
Nolan Larison <nlarison@Nolans-MBP.attlocal.net>
2024-04-05 21:39:12 -0400

    added midterm 2 folder

--- Class/main.asm
+++ Class/main.asm
@@ -29,9 +29,6 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 
-			mov.w #2, R5
-			mov.w #7, R6
-			call #x_times_y
 
 
 main:		jmp main
@@ -40,30 +37,7 @@ main:		jmp main
 
 
 
-;-------------------------------------------------------------------------------
-; Subroutine: x_times_y
-; Input: 	16 bit signed number in R5 and R6
-; Output: 	16 bit signed number in R4
-;-------------------------------------------------------------------------------
-x_times_y:
-			push R5							;pushing data to stack
-			push R6
 
-			clr.w R4						;clearing result register
-
-test:		bit.w #1, R6					;checks least sig bit in R6
-			jz rolls						;if zero, jump to rolls
-			add.w R5, R4					;otherwise, add R5 to output
-rolls:		rla.w R5						;roll R5 left and R6 right
-			rra.w R6
-			bit.w #-1, R6					;bit test with 0xffff
-			jnz test						;and terminate subroutine if
-											;zero is returned due to
-											;no 1s left in R6
-
-			pop R6							;retrieving data from stack
-			pop R5
-			ret
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
